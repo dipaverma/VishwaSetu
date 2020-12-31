fa77ace1326615a093fd4b14b8a521e3b3af3dee
Dipanshu Verma <dipanshu231099>
2020-12-31 18:52:31 +0530

    some changes

--- VishwaSetu/models.py
+++ VishwaSetu/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 class User(models.Model):
     username = models.OneToOneField("username", on_delete=models.CASCADE)
+    password = models.CharField(max_length=30)
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     bio = models.CharField(max_length = 200)
     profile_pic = models.ImageField(upload_to = 'ProfilePicture/')
     date = models.DateTimeField(auto_add_now = True, null=True)
     follow = models.ManyToMany("self")
-
+    
     def __str__(self):
         return self.username
 
 class Post(models.Model):
     image = models.ImageField(upload_to="posts/")
     image_caption = models.CharField(max_length = 200)
-    uploader = models.ForeignKey(User, on_delete = models.CASCADE)
+    uploader = models.ForeignKey(User)
     likes = models.IntegerField()
     
